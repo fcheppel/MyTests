0462674d0e81d3ad1da5101260a6f5cb97da7f3c
Professional <Professional@USER-PC>
2020-11-23 16:49:44 +0300

    first commit

---
+++ Project1/ConsoleApp1/ConsoleApp1/Logic/Processing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Logic
+{
+    public class Processing
+    {
+        public decimal dec;
+
+        public decimal Test()
+        {
+            return 1M;
+        }
+    }
+}
---
+++ Project1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleApp1.Logic;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<decimal> { 1, 2, 3, 3 };
+
+            var proc = new Processing();
+
+            Console.WriteLine("Starting!");
+
+            Console.WriteLine("Count args: " + args.Length);
+
+            Console.WriteLine("Count: "+list.Count);
+
+            Console.WriteLine("Res: "+proc.Test());
+
+            Console.ReadKey();
+        }
+    }
+}
