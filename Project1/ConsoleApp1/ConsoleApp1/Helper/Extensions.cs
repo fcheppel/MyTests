e5bb1b57dbe9be9c9a78a1c67404306f63f1a4fe
Professional <Professional@USER-PC>
2020-11-24 22:54:01 +0300

    Signed-off-by: Professional <Professional@USER-PC>

--- Project1/ConsoleApp1/ConsoleApp1/Helper/Extensions.cs
+++ Project1/ConsoleApp1/ConsoleApp1/Helper/Extensions.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace ConsoleApp1.Helper
 {
-    class Extensions
+    public static string GetDesc(this Enum value)
     {
+        return value
+            .GetType()
+            .GetMember(value.ToString())
+            .FirstOrDefault()?
+            .GetCustomAttribute<DescriptionAttribute>()?
+            .Description
+            ?? value.ToString();
+    }
+
+    public static string ListToString<T>(this IEnumerable<T> l)
+    {
+        return l.Aggregate(new StringBuilder(), (c, n) => c.Append(string.IsNullOrEmpty(c.ToString()) ? "" : ",").Append(n)).ToString();
     }
 }
